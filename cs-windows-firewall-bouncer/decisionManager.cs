874684119f1e8fbc67427fec17b0c48d362dcd10
Sebastien Blot <sebastien@crowdsec.net>
2022-02-07 15:05:34 +0100

    better handling of LAPI errors

--- cs-windows-firewall-bouncer/decisionManager.cs
+++ cs-windows-firewall-bouncer/decisionManager.cs
@@ -28,13 +28,24 @@ public class DecisionsManager
 	public async Task<bool> Run()
     {
 		var decisions = await apiClient.GetDecisions(true);
+		if (decisions == null)
+        {
+			Logger.Error("Could not get initial decisions from LAPI.");
+			return false;
+        }
 		firewall.UpdateRule(decisions);
 		var interval = this.interval * 1000;
 		while (true)
         {
 			decisions = await apiClient.GetDecisions(false);
+			if (decisions == null)
+			{
+				Logger.Error("Could not get decisions from LAPI.");
+				Task.Delay(interval).Wait();
+				continue;
+			}
 			firewall.UpdateRule(decisions);
 			Task.Delay(interval).Wait();
-        }
-    }
+		}
+	}
 }
