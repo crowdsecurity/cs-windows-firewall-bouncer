cf486a6139dfd41b7bcec68146677d8bf5e56663
Sebastien Blot <sebastien@crowdsec.net>
2022-04-15 23:06:08 +0200

    Bump version to 0.0.3

--- cs-windows-firewall-bouncer/apiClient.cs
+++ cs-windows-firewall-bouncer/apiClient.cs
@@ -46,7 +46,7 @@ namespace Api
                 this.apiEndpoint = apiEndpoint + '/';
             }
             client.DefaultRequestHeaders.Add("X-Api-Key", apiKey);
-            client.DefaultRequestHeaders.Add("User-Agent", "cs-windows-fw-bouncer/0.0.2");
+            client.DefaultRequestHeaders.Add("User-Agent", "cs-windows-fw-bouncer/0.0.3");
         }
 
         public async Task<DecisionStreamResponse> GetDecisions(bool startup)
